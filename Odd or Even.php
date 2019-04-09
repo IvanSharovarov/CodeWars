e47681c9f9600e1e26c109e04db3eff8016f2493
IvanSharovarov <sharovarov31@gmail.com>
2019-04-09 21:40:32 +0600

    Odd or Even

---
+++ Odd or Even.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Given an array of numbers (a list in groovy), determine 
+whether the sum of all of the numbers is odd or even.
+
+Give your answer in string format as 'odd' or 'even'.
+
+If the input array is empty consider it as: [0] (array 
+with a zero).
+
+Example:
+odd_or_even([0]); // => 'even'
+odd_or_even([2, 5, 34, 6]); // => 'odd'
+odd_or_even([0, -1, -5]); // => 'even'
+Have fun!
+*/
+function odd_or_even(array $a): string {
+  return array_sum($a) % 2 ? 'odd' : 'even';
+}
